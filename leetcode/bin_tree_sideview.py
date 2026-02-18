fd4204682ee66b5babf8b7d392d5093be6307326
Krzysiek-Mistrz <krzyskuar@gmail.com>
2026-02-18 13:39:07 +0100

    feat: solved medium prb from leetcode regardin bin tree

---
+++ leetcode/bin_tree_sideview.py
@@ -0,0 +1,40 @@
+# Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+# Example 1:
+# Input: root = [1,2,3,null,5,null,4]
+# Output: [1,3,4]
+# logic: simple bfs and appending last elem from level 2 list
+
+from typing import Optional, List
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        result = []
+        queue = deque([root])
+        
+        while queue:
+            level_size = len(queue)
+            
+            for i in range(level_size):
+                node = queue.popleft()
+                
+                if i == level_size - 1:
+                    result.append(node.val)
+                    
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                    
+        return result
